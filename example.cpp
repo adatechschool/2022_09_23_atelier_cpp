656b3064a2128905e6279e279197398b27fe2120
bow <pro.lpascal@gmail.com>
2022-09-23 11:54:35 +0200

    La première classe

--- example.cpp
+++ example.cpp
@@ -2,11 +2,28 @@
 
 
 
+class Person {
+  unsigned int m_age;
+  unsigned int m_size;
+
+public:
+  unsigned int get_size() {
+    return m_size;
+  }
+  unsigned int get_age() {
+    return m_age;
+  }
+  Person(unsigned int a, unsigned int b): m_age(a), m_size(b) {}
+};
+
 
 int main(){
-  char a = 'e';
-  char hello[] = "Hello world\n";
-  std::cout << hello << " tok " << a << std::endl;
+
+  Person person(5, 172);
+
+  // std::cout << person.m_age;
+  std::cout << "Age: " << person.get_age()
+            << std::endl << "Taille: " << person.get_size() << std::endl;
 
   return 0;
 }
